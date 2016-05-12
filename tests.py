12a261aa3e5c689be3f2d3072853eeb0123abc6b
quartata <quartata0@gmail.com>
2016-05-12 13:03:22 -0700

    fixed

--- tests.py
+++ tests.py
@@ -3,7 +3,7 @@
 import subprocess, sys, tempfile
 
 tests = [
-  [b"p\x01\xe42\xe3\xb0", "3", r"\\\o///"]
+  [b"p\x01\xe42\xe3\xb0", "3", b"\\\\\\o///\n"]
 ]
 
 interpreter_path = "python3"
@@ -14,10 +14,12 @@ for test in enumerate(tests):
   try:
     temp = tempfile.NamedTemporaryFile()
     temp.write(test[1][0])
-    if subprocess.check_output("python3 cinnamon-gum.py %s < '%s'" % [temp.name, test[1][1]], shell=True) == test[1][2]:
+    temp.flush()
+    result = subprocess.check_output("python3 cinnamon-gum.py %s <<< '%s'" % (temp.name, test[1][1]), shell=True, executable="/bin/bash") 
+    if result == test[1][2]:
       pass
     else:
-      print("Test " + str(test[0] + 1) + " " + str(test[1][0]) +" failed (incorrect output)\n")
+      print("Test " + str(test[0] + 1) + " " + str(test[1][0]) +" failed (incorrect output %s)\n" % result)
   except Exception as e:
     print("Test " + str(test[0] + 1) + " " + str(test[1][0]) +" failed (exception %s)\n" % e)
 

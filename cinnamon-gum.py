62950c5573ccbf145ebc4daf597cef0be2697ceb
quartata <quartata0@gmail.com>
2016-07-26 16:08:47 -0700

    Added backtick, cleaned up code significantly, added escape handling, added i mode

--- cinnamon-gum.py
+++ cinnamon-gum.py
@@ -13,57 +13,78 @@ def bb96encode(code, a = 0, s = []):
     a //= 96
   return bytes(s)
 
-with open(sys.argv[1], 'rb') as file:
-  code = file.read()
-  
-try:
-  i = input()
-except EOFError:
-  i = ""
-
-if hashlib.sha256(code).hexdigest() == "bca4894ae7cf4919e3b3977583df930c8f4bf5b75c8bf5ada9de1d9607ef846b":
-  exec(code)
-else:
+def decompress(code):
   mode = chr(code[0])
   code = code[1:]
   try:
-    o = zlib.decompress(code, -zlib.MAX_WBITS)
+    decompressed_code = zlib.decompress(code, -zlib.MAX_WBITS)
   except:
     try:
-      o = lzma.decompress(code, format=lzma.FORMAT_RAW, filters=[{'id': lzma.FILTER_LZMA2, 'preset': 9 | lzma.PRESET_EXTREME}])
+      decompressed_code = lzma.decompress(code, format=lzma.FORMAT_RAW, filters=[{'id': lzma.FILTER_LZMA2, 'preset': 9 | lzma.PRESET_EXTREME}])
     except:
-      o = bb96encode(code)
+      decompressed_code = bb96encode(code)
 
-  o = "".join(map(chr,o))
+  return mode, "".join(map(chr, decompressed_code)) 
+
+def get_input():
+  try:
+    return input()
+  except EOFError:
+    return ""
+
+def execute(mode, code, input_str):
+  result = ""
 
   if mode == "l":
-    rows = [row.split("&") for row in o.split(";")]
-    table = {}
-    for row in rows:
-      table.update(dict(zip(row[:-1],[row[-1]]*(len(row)-1))))
-    print(table[i], end="")
+     rows = [re.split(r"(?<![^\\]\\)&", row) for row in re.split(r"(?<![^\\]\\);", code)]
+     table = {}
+     for row in rows:
+       table.update(dict(zip(row[:-1],[row[-1]]*(len(row)-1))))
+     result = table[i]
   elif mode == "f":
-    print(o % ast.literal_eval(i), end="")
+     result = code % ast.literal_eval(input_str)
   elif mode == "g":
-    for string in exrex.generate(o):
+    for string in exrex.generate(code):
       print(string)
+    return # Generate is always terminal
   elif mode == "h":
-    inputStr = ast.literal_eval(i)
-    if type(inputStr) is str:
-      inputStr = re.escape(inputStr)
-    for string in exrex.generate(o % inputStr):
+    if type(input_str) is str:
+      input_str = re.escape(input_str)
+    for string in exrex.generate(code % input_str):
       print(string)
+    return
   elif mode == "p":
-    print(re.sub(r"~(.)",r"\1" * ast.literal_eval(i),o)) 
+    result = re.sub(r"~(.)",r"\1" * ast.literal_eval(input_str), code) 
   elif mode == "e":
-    rows = [row.split("&") for row in o.split(";")]
+    rows = [re.split(r"(?<![^\\]\\)&", row) for row in re.split(r"(?<![^\\]\\);", code)]
     table = {}
     for row in rows:
       table.update(dict(zip(row[:-1],[row[-1]]*(len(row)-1))))
     for char in i:
-      print(table[i], end="")
+      result += table[i]
   elif mode == "s":
-    subs = o.split("&")
-    print(re.sub(subs[0],subs[1],i))
+    subs = re.split(r"(?<![^\\]\\)&", code)
+    result = re.sub(subs[0],subs[1],i)
+  elif mode == "i":
+    result = code + input_str
   else:
-    print(o, end="")
+    result = code
+
+  if result[0] == "`":
+    input_pieces = re.split(r"(?<![^\\]\\)!", result)
+    if len(input_pieces) >= 2:
+      execute(result[1], input_pieces[0][2:], "!".join(input_pieces[1:]))
+    else:
+      execute(result[1], result[2:], get_input())
+  else:
+    print(result)
+
+if __name__ == "__main__":
+  with open(sys.argv[1], 'rb') as file:
+    code = file.read()
+    i = get_input()
+
+    if hashlib.sha256(code).hexdigest() == "bca4894ae7cf4919e3b3977583df930c8f4bf5b75c8bf5ada9de1d9607ef846b":
+      exec(code)
+    else:
+      execute(*(decompress(code) + (i,)))
--- tests.py
+++ tests.py
@@ -4,7 +4,8 @@ import subprocess, sys, tempfile
 
 tests = [
   [b"p\x01\xe42\xe3\xb0", "3", b"\\\\\\o///\n"],
-  [b"from math import factorial as F#\ntry:n=int(i)-1;print(n*(F(n)%-~n==n))\nexcept:print(sum(map(int,i.split())))", "5", b"4\n"]
+  [b"from math import factorial as F#\ntry:n=int(i)-1;print(n*(F(n)%-~n==n))\nexcept:print(sum(map(int,i.split())))", "5", b"4\n"],
+  [b"l\x1f2\xe01]\x90I\x85\xdf\x10T\x03d-\xc3J\xdb8\xd3", "1", b"aa\n"]
 ]
 
 interpreter_path = "python3"

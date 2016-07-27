78a3e18b60a33913e376460844237663df0707f9
quartata <quartata0@gmail.com>
2016-07-27 10:10:52 -0700

    Added PCRE

--- cinnamon-gum.py
+++ cinnamon-gum.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import ast, exrex, hashlib, lzma, re, sys, zlib
+import ast, exrex, hashlib, lzma, pcre, sys, zlib
 
 def bb96encode(code, a = 0, s = []):
   code = list(code)
@@ -36,7 +36,7 @@ def execute(mode, code, input_str):
   result = ""
 
   if mode == "l":
-     rows = [re.split(r"(?<![^\\]\\)&", row) for row in re.split(r"(?<![^\\]\\);", code)]
+     rows = [pcre.split(r"(?<![^\\]\\)&", row) for row in pcre.split(r"(?<![^\\]\\);", code)]
      table = {}
      for row in rows:
        table.update(dict(zip(row[:-1],[row[-1]]*(len(row)-1))))
@@ -52,57 +52,57 @@ def execute(mode, code, input_str):
     return # Generate is always terminal
   elif mode == "h":
     if type(input_str) is str:
-      input_str = re.escape(input_str)
+      input_str = pcre.escape(input_str)
     for string in exrex.generate(code % input_str):
       print(string)
     return
   elif mode == "p":
     literal = ast.literal_eval(input_str)
     if isinstance(literal, int):
-      result = re.sub(r"(?<![^\\]\\)~(.+?)(?<![^\\]\\)~",r"\1" * literal, code, flags=re.DOTALL) 
+      result = pcre.sub(r"(?<![^\\]\\)~(.+?)(?<![^\\]\\)~",r"\1" * literal, code, flags=pcre.DOTALL) 
     else:
-      result = re.sub(r"(?<![^\\]\\)%(.+?)(?<![^\\]\\)%",r"\1" * literal[1], re.sub(r"~(.+?)~",r"\1" * literal[0], code, flags=re.DOTALL), flags=re.DOTALL)
+      result = pcre.sub(r"(?<![^\\]\\)%(.+?)(?<![^\\]\\)%",r"\1" * literal[1], pcre.sub(r"~(.+?)~",r"\1" * literal[0], code, flags=pcre.DOTALL), flags=pcre.DOTALL)
   elif mode == "e":
-    rows = [re.split(r"(?<![^\\]\\)&", row) for row in re.split(r"(?<![^\\]\\);", code)]
+    rows = [pcre.split(r"(?<![^\\]\\)&", row) for row in pcre.split(r"(?<![^\\]\\);", code)]
     table = {}
     for row in rows:
       table.update(dict(zip(row[:-1],[row[-1]]*(len(row)-1))))
     for char in i:
       result += table[i]
   elif mode == "o":
-    pieces = re.split(r"(?<![^\\]\\)`", code)
+    pieces = pcre.split(r"(?<![^\\]\\)`", code)
     print(pieces[0])
     result = "`" + "`".join(pieces[1:])
   elif mode == "s":
-    pieces = re.split(r"(?<![^\\]\\)`", code)
-    subs = re.split(r"(?<![^\\]\\)&", pieces[0])
+    pieces = pcre.split(r"(?<![^\\]\\)`", code)
+    subs = pcre.split(r"(?<![^\\]\\)&", pieces[0])
     sub_length = len(subs)
 
     for i in range(0, len(subs), 2):
-      input_str = re.sub(subs[i], subs[i + 1], input_str)
+      input_str = pcre.sub(subs[i], subs[i + 1], input_str)
 
     if len(pieces) > 1:
       result = "`" + "`".join(pieces[1:])
     else:
       result = input_str
   elif mode == "d":
-    pieces = re.split(r"(?<![^\\]\\)`", code)
-    subs = re.split(r"(?<![^\\]\\)&", pieces[0])
+    pieces = pcre.split(r"(?<![^\\]\\)`", code)
+    subs = pcre.split(r"(?<![^\\]\\)&", pieces[0])
     
     for sub in subs:
-      input_str = re.sub(sub, "", input_str)
+      input_str = pcre.sub(sub, "", input_str)
 
     if len(pieces) > 1:
       result = "`" + "`".join(pieces[1:])
     else:
       result = input_str
   elif mode == "S":
-    pieces = re.split(r"(?<![^\\]\\)`", code)
-    subs = re.split(r"(?<![^\\]\\)&", pieces[0])
+    pieces = pcre.split(r"(?<![^\\]\\)`", code)
+    subs = pcre.split(r"(?<![^\\]\\)&", pieces[0])
     sub_length = len(subs)
     output = input_str
     for i in range(0, len(subs), 2):
-      output = re.sub(subs[i], subs[i + 1], output)
+      output = pcre.sub(subs[i], subs[i + 1], output)
     if len(pieces) > 1:
       result = "`" + "`".join(pieces[1:])
     else:
@@ -116,7 +116,7 @@ def execute(mode, code, input_str):
     result = code
 
   if len(result) > 0 and result[0] == "`":
-    input_pieces = re.split(r"(?<![^\\]\\)!", result)
+    input_pieces = pcre.split(r"(?<![^\\]\\)!", result)
     if len(input_pieces) >= 2:
       execute(result[1], input_pieces[0][2:], "!".join(input_pieces[1:]))
     else:
@@ -125,6 +125,7 @@ def execute(mode, code, input_str):
     print(result)
 
 if __name__ == "__main__":
+  pcre.enable_re_template_mode()
   with open(sys.argv[1], 'rb') as file:
     code = file.read()
     i = get_input("")
